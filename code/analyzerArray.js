dff0d08eeccd049bdab5f4dcdba0178e7fe9f5f8
Hassane24 <hsisane5@gmail.com>
2022-04-12 03:54:18 +0100

    array function done

--- code/analyzerArray.js
+++ code/analyzerArray.js
@@ -1,3 +1,15 @@
-function arrayAnalyzer(array) { }
+function arrayAnalyzer(array) {
+  let min = array[0];
+  let max = array[0];
+  let average = 0;
+  let length = array.length;
+  array.forEach((element) => {
+    if (element < min) min = element;
+    if (max < element) max = element;
+    average += element;
+  });
+  average = average / length;
+  return { min, max, average, length };
+}
 
-export { arrayAnalyzer }
+export { arrayAnalyzer };
--- test/analyzerArray.test.js
+++ test/analyzerArray.test.js
@@ -1,1 +1,17 @@
 import { arrayAnalyzer } from "../code/analyzerArray";
+
+test("Checks for the minimum value", () => {
+  expect(arrayAnalyzer([1, 8, 3, 4, 2, 6]).min).toBe(1);
+});
+
+test("Checks for the maximum value", () => {
+  expect(arrayAnalyzer([1, 8, 3, 4, 2, 6]).max).toBe(8);
+});
+
+test("Checks for the average value", () => {
+  expect(arrayAnalyzer([1, 8, 3, 4, 2, 6]).average).toBe(4);
+});
+
+test("Checks for the length of the array", () => {
+  expect(arrayAnalyzer([1, 8, 3, 4, 2, 6]).length).toBe(6);
+});
