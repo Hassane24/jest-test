0857bf767234ff2c7f839903f173d9552d155c66
Hassane24 <hsisane5@gmail.com>
2022-04-12 02:15:31 +0200

    calculator obj done

--- code/calculator.js
+++ code/calculator.js
@@ -1,3 +1,23 @@
-const calculator = {}
+const calculator = {
+  add: (a, b) => {
+    a = Number(a);
+    b = Number(b);
+    return a + b;
+  },
 
-export { calculator }
+  substract: (a, b) => {
+    return a - b;
+  },
+
+  multiply: (a, b) => {
+    return a * b;
+  },
+
+  divide: (a, b) => {
+    b = Number(b);
+    if (b === 0) return "ERROR";
+    return a / b;
+  },
+};
+
+export { calculator };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,1 +1,41 @@
 import { calculator } from "../code/calculator";
+
+test("Adds two numbers", () => {
+  expect(calculator.add(3, 2)).toBe(5);
+});
+
+test("Handles strings", () => {
+  expect(calculator.add("5", "6")).toBe(11);
+});
+
+test("Adds two decimal numbers", () => {
+  expect(calculator.add(2.5, 2.5)).toBeCloseTo(5);
+});
+
+test("Subtracts two numbers", () => {
+  expect(calculator.substract(10, 5)).toBe(5);
+});
+
+test("Handles strings", () => {
+  expect(calculator.substract("5", "6")).toBe(-1);
+});
+
+test("Multiplies two numbers", () => {
+  expect(calculator.multiply(10, 5)).toBe(50);
+});
+
+test("Handles strings", () => {
+  expect(calculator.multiply("5", "6")).toBe(30);
+});
+
+test("Divides two numbers", () => {
+  expect(calculator.divide(10, 5)).toBe(2);
+});
+
+test("Cant devide by zero", () => {
+  expect(calculator.divide(10, 0)).toBe("ERROR");
+});
+
+test("Handles strings ", () => {
+  expect(calculator.divide("10", "5")).toBe(2);
+});
