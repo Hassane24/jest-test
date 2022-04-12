c82527ad1bf63ee05c10d18f8b26afcaa26d5cb4
Hassane24 <hsisane5@gmail.com>
2022-04-12 05:17:45 +0100

    caesarCipher function done

--- test/caesarCipher.test.js
+++ test/caesarCipher.test.js
@@ -1,1 +1,15 @@
 import { caesarCipher } from "../code/caesarCipher";
+
+test("Shifts letters by a given amount", () => {
+  expect(caesarCipher("hello world", 5)).toBe("mjqqt btwqi");
+});
+
+test("Handles negative numbers", () => {
+  expect(caesarCipher("hello world", -10)).toBe("xubbe mehbt");
+});
+
+test("Handles large positive shifts", () =>
+  expect(caesarCipher("hello world", 1000)).toBe("tqxxa iadxp"));
+
+test("Handles capitals", () =>
+  expect(caesarCipher("HeLlO WoRlD", 5)).toBe("MjQqT BtWqI"));
